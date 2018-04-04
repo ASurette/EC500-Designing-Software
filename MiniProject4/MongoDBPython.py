e1711217f5cac340e1412e7e512a568e0db94bf7
ASurette <asurette@bu.edu>
2018-04-04 16:34:16 -0400

    functionality almost done
    TwitterDownload has a minor change where it also returns a list of all the image links.
    
    MongoDBPython now adds new images to the database if there are any

--- MiniProject4/MongoDBPython.py
+++ MiniProject4/MongoDBPython.py
@@ -26,17 +26,21 @@ def DatabaseUpdate():
 
     else:
 
-        document = td.find_one({"Twitter Handle": twitter_handle});
+        document = db.td.find_one({"Twitter Handle": twitter_handle});
 
         if( document != None ): #if the twitter handle has been called before and is in the database
             #reference the list of image links and compare the new list to the old list
 
             potential_new_links_list = [];#an array to save links for new images that are checked against old ones
 
-            saved_images_list = td.find_one({"Twitter Handle": twitter_handle}).get("Image Links");
+            saved_images_list = db.td.find_one({"Twitter Handle": twitter_handle}).get("Image Links");
 
             #go through all the new image links, compare them against the old links, if it is not already in the old links put it in the array
             #this array will then be added to the database
+
+            index_list = [];#an empty list that will be used to store the index of new images
+
+            index_count = 0;# a count so that if there are new picture we can save the index in the list for them, use this to index the tags in the other list
             for val in picTweets:
 
                 count = 0;
@@ -47,39 +51,53 @@ def DatabaseUpdate():
 
                         count = count + 1;
 
-                #end for link in x
+                #end for link in saved_images_list
                 if(count == len(saved_images_list)):#if there were no matches
                     potential_new_links_list.append(val);
+                    index_list.append(index_count);
 
+                index_count = index_count + 1;
             #end for val in pic tweets
-
-            print(potential_new_links_list);
 
             if(len(potential_new_links_list) == 0):#if no new values
                 #do nothing
+                print("Now new images found");
                 return "No new images found";
             else:#at least one new value
                 #add the new images to the end of the saved images list
                 links_list = saved_images_list+potential_new_links_list;
 
-                print("Links list is: ", links_list);
+                db.td.update_one({"Twitter Handle": twitter_handle}, {'$set': {"Image Links": links_list}});#updates the image links list with the new images
 
+                if(len(index_list) != 0):
 
+                    num_images = db.td.find_one({"Twitter Handle": twitter_handle}).get("Number of Images Saved");
+
+                    for x in range (0, len(index_list)):
+                        name = "image" + str(x + num_images);
+                        tags = twitterList[index_list[x]];
+                        #adds the new imageX and its tags to the document
+                        db.td.update_one({"Twitter Handle": twitter_handle}, {'$set': {name: tags}});
+
+                    #end for loop
+
+                    #need to update the number of images as well
+                    db.td.update_one({"Twitter Handle": twitter_handle}, {'$set': {"Number of Images Saved": len(links_list)}});
 
         else:   #brand new value for the database
             list_length = len(twitterList) - 1;#the length of the output -1 because the last element in the list is the filepath to the video
+            data["Number of Images Saved"] = list_length;
 
             for i in range (0, list_length):
                 temp_dict = {};#make an empty dict that we will fill with data and merge with data
                 temp_name = "image" + str(i);#the first part of the dict which depends on the image number
                 temp_name2 = twitterList[i];#index into the output of TwitterDownload which is a list of lists with the google vision data
                 temp_dict = {temp_name: temp_name2};#add the image name and the description for them into the temp dict
-                data.update(temp_dict);#adds the temp dict to the dict
+                data.update(temp_dict);#adds the temp dict to the dict that will be added to the database
                 #end of the for loop
 
-        data["Number of Images Saved"] = list_length;
-        print(data);
-        td.insert_one(data);
-
+        db.td.insert_one(data);
 
 DatabaseUpdate();
+
+#db.td.delete_many({});#deletes all documents, used in testing
