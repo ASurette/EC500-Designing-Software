6bf32250096829b1121956c8379c76f496895092
ASurette <asurette@bu.edu>
2018-04-07 21:47:09 -0400

    Create MongoPhase3.py

---
+++ MiniProject4/MongoPhase3.py
@@ -0,0 +1,31 @@
+import MongoDBPython
+
+from MongoDBPython import DatabaseUpdate
+
+DatabaseUpdate('katyperry');
+DatabaseUpdate('justinbieber');
+DatabaseUpdate('BarackObama');
+DatabaseUpdate('rihanna');
+DatabaseUpdate('taylorswift13');
+print("done 5");
+DatabaseUpdate('ladygaga');
+DatabaseUpdate('TheEllenShow');
+DatabaseUpdate('Cristiano');
+DatabaseUpdate('Youtube');
+DatabaseUpdate('jtimberlake');
+print('done 10');
+DatabaseUpdate('twitter');
+DatabaseUpdate('KimKardashian');
+DatabaseUpdate('britneyspears');
+DatabaseUpdate('ArianaGrande');
+DatabaseUpdate('selenagomez');
+print('done 15');
+DatabaseUpdate('ddlovato');
+DatabaseUpdate('cnnbrk');
+DatabaseUpdate('shakira');
+DatabaseUpdate('jimmyfallon');
+DatabaseUpdate('realDonaldTrump');
+print('done 20');
+
+from MongoDBPython import TotalTags
+data = TotalTags();
